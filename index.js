c4cb570f81c6d9169a375276888d26668f2da98f
vengi57 <vengithiyagarajan@gmail.com>
2019-11-05 14:32:02 +0530

    1 file modified

--- index.js
+++ index.js
@@ -24,17 +24,6 @@ class App extends Component {
       website:undefined
     }
   }
-  
-  // componentDidMount(){
-  //   axios
-  //   .get("https://jsonplaceholder.typicode.com/users")
-  //   .then(response=>{
-  //     this.setState({
-  //       array:response.data,
-  //       isLoading:true
-  //     })
-  //   })
-  // }
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res=>res.json())
@@ -72,59 +61,5 @@ class App extends Component {
     );
   }
 }
-// class App extends React.Component {
-//   // State will apply to the posts object which is set to loading by default
-//   state = {
-//     results: [],
-
-//     isLoading: true,
-//     errors: null
-//   };
-//   // Now we're going to make a request for data using axios
-//   getPosts() {
-//     axios
-//       // This is where the data is hosted
-//       .get("https://randomuser.me/api/")
-//       // Once we get a response and store data, let's change the loading state
-//       .then(response => {
-//         this.setState({
-//           results: response.data.results,
-//           isLoading: false
-//         });
-//       })
-//       // If we catch any errors connecting, let's update accordingly
-//       .catch(error => this.setState({ error, isLoading: false }));
-//   }
-//   // Let's our app know we're ready to render the data
-//   componentDidMount() {
-//     this.getPosts();
-//   }
-//   // Putting that data to use
-//   render() {
-//     const { isLoading, results } = this.state;
-//     return (
-//       <React.Fragment>
-//         <h2>Random Post</h2>
-//         <div>
-//           {!isLoading ? (
-//             results.map(post => {
-//               const { gender, city, content,picture } = post;
-//               return (
-//                 <div>
-//                   <h2>{gender}</h2>
-//                   <p>{city}</p>
-//                   <img src={picture.medium}/>
-//                   <hr />
-//                 </div>
-//               );
-//             })
-//           ) : (
-//             <p>Loading...</p>
-//           )}
-//         </div>
-//       </React.Fragment>
-//     );
-//   }
-// }
 
  render(<App />, document.getElementById('root'));
