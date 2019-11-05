4a03f95898f2da382344958d5fe0e91a2fd38735
vengi57 <vengithiyagarajan@gmail.com>
2019-11-05 23:31:46 +0530

    Hooks useEffect and useState using api

--- components/HooksUpdate.js
+++ components/HooksUpdate.js
@@ -6,27 +6,35 @@ import { render } from 'react-dom';
 export default function HooksUpdate()
 {
   const [data, setData] = useState({ hits: [] });
-  const [query,setQuery] = useState('redux');
-  const [search, setSearch] = useState('redux');
+  const [query,setQuery] = useState(''); 
+  const [search,setSearch] = useState('');
+  // const [search, setSearch] = useState('');
 
-  
-   useEffect(async () => {
+const handleSubmit = e =>
+{
+  e.preventDefault();
+  setSearch(query);
+  // setSearch(query);
+}
+const apifetch= async()=>
+{
     const result = await axios(
-      `https://hn.algolia.com/api/v1/search?query=${query}`,
+      `https://hn.algolia.com/api/v1/search/?query=${query}`,
     );
     setData(result.data);
+}
+  
+   useEffect(() => {
+    apifetch();
   }, [search]);
   return (
     <div>
     <input
         type="text"
         value={query}
-        onChange={event => setQuery(console.log(event.target.value))}
+        onChange={e => setQuery(e.target.value )}
       />
-      <button type="button" onClick={() => setSearch(query)}>
-        Search
-      </button>
-
+      <button type="submit" onClick={handleSubmit}>Search</button>
      <ul>
         {data.hits.map(item => (
           <li key={item.objectID}>
